484f9899d67f60a2a02a641aacb012e4fa22b14c
StrandedHobo <44891726+StrandedHobo@users.noreply.github.com>
2018-12-23 02:55:41 +0100

    1.0

---
+++ js/menu.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $('a').click(function(){
+    var selected = $(this);
+    $('a').removeClass('active');
+    $(selected).addClass('active')
+  });
+
+  var $a = $('.a'),
+  $b = $('.b'),
+  $c = $('.c'),
+  $d = $('.d'),
+  $home = $('.home'),
+  $aboutme = $('.about-me');
+  $gallery = $('.gallery');
+  $contact = $('.contact');
+
+  $a.click(function(){
+    $home.fadeIn();
+    $aboutme.fadeOut();
+    $gallery.fadeOut();
+    $contact.fadeOut();
+  });
+  $b.click(function(){
+    $aboutme.fadeIn();
+    $home.fadeOut();
+    $gallery.fadeOut();
+    $contact.fadeOut();
+  });
+  $c.click(function(){
+    $gallery.fadeIn();
+    $home.fadeOut();
+    $aboutme.fadeOut();
+    $contact.fadeOut();
+  });
+  $d.click(function(){
+    $contact. fadeIn();
+    $aboutme.fadeOut();
+    $home.fadeOut();
+    $gallery.fadeOut();
+  });
+});

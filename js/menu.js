0ce100819813d7b1875cd07249aed2b7c1c2f6a8
StrandedHobo <44891726+StrandedHobo@users.noreply.github.com>
2019-05-21 09:07:47 +0200

    changed??

--- js/menu.js
+++ js/menu.js
@@ -13,29 +13,39 @@ $(document).ready(function(){
   $aboutme = $('.about-me');
   $gallery = $('.gallery');
   $contact = $('.contact');
+  $submitted = $('.submitted');
+  $submitbutton = $('.submitbutton');
 
   $a.click(function(){
     $home.fadeIn    ();
     $aboutme.fadeOut();
     $gallery.fadeOut();
     $contact.fadeOut();
+    $submitted.fadeOut();
   });
   $b.click(function(){
     $aboutme.fadeIn ();
     $home.fadeOut   ();
     $gallery.fadeOut();
     $contact.fadeOut();
+    $submitted.fadeOut();
   });
   $c.click(function(){
     $gallery.fadeIn ();
     $home.fadeOut   ();
     $aboutme.fadeOut();
     $contact.fadeOut();
+    $submitted.fadeOut();
   });
   $d.click(function(){
     $contact. fadeIn();
     $aboutme.fadeOut();
     $home.fadeOut   ();
     $gallery.fadeOut();
+    $submitted.fadeOut();
+  });
+  $submitbutton.click(function(){
+    $contact.fadeOut();
+    $submitted.fadeIn();
   });
 });
